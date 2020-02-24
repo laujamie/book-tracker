1d7b623646007fa2b7ccd7e612eb4fd6dd5f9f04
laujamie <36782341+laujamie@users.noreply.github.com>
2020-02-24 01:11:49 +0000

    Add models and controllers for mongoose

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const app = express();
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 8000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello from MERN");
-});
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
